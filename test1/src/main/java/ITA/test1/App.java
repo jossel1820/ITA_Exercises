ce644cc84d7cbb3522d765bdb6c7699dba92f792
escobjo <escobjo@ZHA-ITA006-W7.corp.oocl.com>
2017-07-31 15:33:22 +0800

    update1

--- test1/src/main/java/ITA/test1/App.java
+++ test1/src/main/java/ITA/test1/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String word = "Hallo ! ^___^ " ;
+        //System.out.println( "Hello World!" );
+        
+        for(int i=0; i <= 5; i++) {
+        	System.out.print(word);
+        }
     }
 }
